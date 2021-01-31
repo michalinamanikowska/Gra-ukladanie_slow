87a26b1f2e041c87d7d91035fd15b3942cc845eb
mbekas <michalbekas.99@gmail.com>
2021-01-31 23:30:36 +0100

    clean

--- Gra_slowa/build-Client-Desktop-Debug/moc_chatclient.cpp
+++ Gra_slowa/build-Client-Desktop-Debug/moc_chatclient.cpp
@@ -6,7 +6,7 @@
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../Gra_slowa/Client/chatclient.h"
+#include "../../Client/chatclient.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
