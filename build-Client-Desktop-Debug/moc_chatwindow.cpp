9ed8efcbdc1bfcb44ad75898f2ccdb22590ab488
michalinamanikowska <misia.manikowska@gmail.com>
2021-01-31 23:50:21 +0100

    name

--- build-Client-Desktop-Debug/moc_chatwindow.cpp
+++ build-Client-Desktop-Debug/moc_chatwindow.cpp
@@ -6,7 +6,7 @@
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../../Client/chatwindow.h"
+#include "../Client/chatwindow.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
